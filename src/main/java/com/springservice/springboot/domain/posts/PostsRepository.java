2b9491c35d84f33bcaa7c7444dde729799298fae
오한별 <suriessay@naver.com>
2020-04-01 23:10:12 +0900

    gradle jpa config postsRpository 작성

---
+++ src/main/java/com/springservice/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.springservice.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+}
