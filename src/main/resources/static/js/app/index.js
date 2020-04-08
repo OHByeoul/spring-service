5015427ad13611d1acc1d3730502a03f5840894c
오한별 <suriessay@naver.com>
2020-04-08 20:05:58 +0900

    글 삭제 api, 기능, 화면 작성

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -8,6 +8,10 @@ var main = {
         $('#btn-update').on('click',function(){
             _this.update();
         })
+
+        $('#btn-delete').on('click',function(){
+             _this.delete();
+        })
     },
 
     save : function(){
@@ -51,6 +55,23 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error))
         });
+    },
+
+    delete : function(){
+        let id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function(){
+            alert('글이 삭제 되었습니다.');
+            location.href = '/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        })
+
     }
 };
 
